1894b9d1aa8b69704ba1f816b2b1eaf7987a7c99
andiniulya <ahanifahulya@yahoo.com>
2015-09-21 17:49:17 +0700

    Update main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -44,15 +44,42 @@ int main()
         break;
 
         case 4:
-            //DELETEFIRST ULYA
+            void deleteFirst (list &l, address p)
+            {
+                if(next(first(l))==nil)
+                {
+                    first(l)=nil;
+                }
+                else
+                {
+                    p=first(l);
+                    first(l)=next(p);
+                    next(p)=nil;
+                }
+            };
             break;
 
         case 5:
-            //DELETELAST ULYA
+            void deleteLast (list &l, address p)
+            {
+                address q;
+                while (next(p)!=nil)
+                {
+                    q=p;
+                    p=next(p);
+                }
+                next(q)=nil;
+                dealokasi(p);
+            };
             break;
 
         case 6:
-           //DELETEAFTER ULYA
+           void deleteafter(address q, address *p)
+           {
+            next(q)=next(*p);
+            next(*p)=NULL;
+            dealokasi(*p);
+            };
             break;
 
         case 7:
