4eb1640c508d21d010eea617be8d384f5ed43af6
wendianto <wendianto.96@gmail.com>
2015-09-21 16:58:07 +0700

    Update list.cpp

--- LinkedList/list.cpp
+++ LinkedList/list.cpp
@@ -1,1 +1,101 @@
-tes
+#include "list.h"
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+//SEMANGAT !!
+
+infotype x;
+
+void createList (list *l)
+{
+    (*l).first = NULL;
+};
+
+void dealokasi (address p)
+{
+    free(p);
+};
+
+address alokasi (infotype x)
+{
+    address p= new elmlist;
+    p = (address) malloc (sizeof(elmlist));
+    info(p)= x;
+    next(p) = NULL;
+    return p;
+};
+
+void insertFirst (list *l, address p)
+{
+
+};
+
+void insertLast (list *l, address p)
+{
+  
+};
+
+
+void deleteFirst (list *l, address p)
+{
+   
+};
+
+void deleteLast (list *l, address p)
+{
+   
+};
+
+
+address searchBuku(list l, infotype x)
+{
+   
+};
+
+
+void input_data (infotype *x)
+{
+    cout <<"ID : ";
+    cin>> (*x).ID;
+    cout<<endl<<"Input Judul : ";
+    cin>> (*x).judul;
+    cout<<endl<<"Input Pengarang : ";
+    cin>> (*x).pengarang;
+    cout<<endl<<"Input Penerbit : ";
+    cin>> (*x).penerbit;
+    cout<<endl<<"Input Jenis : ";
+    cin>> (*x).jenis;
+    cout<<endl<<"Stock : ";
+    cin>> (*x).stock;
+};
+
+void viewList (list l)
+{
+    address p;
+    p = ((l).first);
+    if (p==NULL)
+    {
+        cout<<"KOSONG";
+        getch();
+    }
+    else
+    {
+        while (p != NULL)
+        {
+            cout << "ID Buku : " << (info(p)).ID<<endl;
+            cout << " Judul : " << (info(p)).judul<<endl;
+            cout << " Penerbit : " << (info(p)).penerbit<<endl;
+            cout << " Pengarang : " << (info(p)).pengarang<<endl;
+            cout << " Jenis : " << (info(p)).jenis<<endl;
+            cout << " Stock : " << (info(p)).stock<<endl;
+            cout<<endl;
+            p = next(p);
+        }
+        getch();
+    }
+};
+
+
