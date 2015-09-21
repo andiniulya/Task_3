7d021082fa3b14f896a634fbf6dbd06edb6466d4
andiniulya <ahanifahulya@yahoo.com>
2015-09-21 17:04:25 +0700

    Update list.cpp
    bener teu?

--- LinkedList/list.cpp
+++ LinkedList/list.cpp
@@ -1,1 +1,31 @@
-tes
+void deleteFirst (list &l, address p)
+{
+    if(next(first(l))==nil)
+    {
+        first(l)=nil;
+    }
+    else
+    {
+        p=first(l);
+        first(l)=next(p);
+        next(p)=nil;
+    }
+};
+void deleteLast (list &l, address p)
+{
+    address q;
+    while (next(p)!=nil)
+    {
+        q=p;
+        p=next(p);
+    }
+    next(q)=nil;
+    dealokasi(p);
+};
+
+void deleteafter(address q, address *p)
+{
+  next(q)=next(*p);
+  next(*p)=NULL;
+  dealokasi(*p);
+};
