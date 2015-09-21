db12a2765b4bd39a67159af173a85e051cb54f09
andiniulya <ahanifahulya@yahoo.com>
2015-09-21 17:05:26 +0700

    Update list.h

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,8 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
-
+void deletefirst(list *l, address p);
+void deletelast(list *l, address p);
+void deleteafter(address *p);
 
 #endif // LIST_H_INCLUDED
