8b6007a745a2322db0ccb376cdb0fbfa67ee5a6a
andiniulya <ahanifahulya@yahoo.com>
2015-09-21 17:36:38 +0700

    Update main.cpp
    bener?

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -21,7 +21,7 @@ int main()
         cout <<endl;
         cout <<"1. Insert Buku First      "                      <<endl;
         cout <<"2. Insert Buku Last       "                      <<endl;
-        cout <<"3. Insert Buku After "                 <<endl;
+        cout <<"3. Insert Buku After    "                 <<endl;
         cout <<"4. Delete Buku First      "                      <<endl;
         cout <<"5. Delete Buku Last       "                      <<endl;
          cout <<"6. Delete Buku After "                 <<endl;
@@ -44,15 +44,42 @@ int main()
         break;
 
         case 4:
-            //DELETEFIRST ULYA
+            void deleteFirst (list &l, address p)
+            {
+                if(next(first(l))==nil)
+                {
+                    first(l)=nil;
+                }
+                else
+                {
+                    p=first(l);
+                    first(l)=next(p);
+                    next(p)=nil;
+                }
+            };
             break;
 
         case 5:
-            //DELETELAST ULYA
+            void deleteLast (list &l, address p)
+            {
+                address q;
+                while (next(p)!=nil)
+                {
+                q=p;
+                p=next(p);
+                }
+                next(q)=nil;
+                dealokasi(p);
+            };
             break;
 
         case 6:
-           //DELETEAFTER ULYA
+           void deleteafter(address q, address *p)
+           {
+            next(q)=next(*p);
+            next(*p)=NULL;
+            dealokasi(*p);
+            };
             break;
 
         case 7:
